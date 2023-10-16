3286d0b69782e5dd61fc62fa42534f84c7de6e2b
riyad <hifazakhatun1108@gmail.com>
2023-10-16 10:55:41 +0600

    dep

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -17,14 +17,14 @@
         <div class="mt-8 flex flex-wrap justify-center gap-4">
           <a
             class="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-            href=""
+            href="https://services.mobilecomusa.com/user/login"
           >
             LOGIN
           </a>
 
           <a
             class="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
-            href=""
+            href="https://services.mobilecomusa.com/user/register"
           >
             REGISTER
           </a>
